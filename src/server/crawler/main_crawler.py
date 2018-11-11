5627d78e39e1bd6e7d0084056f76943d56ddf1b6
Alon Singer <singer.alon@gmail.com>
2018-11-11 12:14:32 +0200

    Commit

---
+++ src/server/crawler/main_crawler.py
@@ -0,0 +1,76 @@
+import sys
+
+import play_scraper
+import random
+import string
+
+from ..utils.db.tools  import db_utils
+
+
+def init_applications_table():
+    db_utils.exec_command("DROP TABLE applications")
+    db_utils.exec_command(
+        "CREATE TABLE applications (id serial not null primary key, name text, developer text, category name, dev_url text , pp_url text )")
+
+
+def id_generator(size=6, chars=string.ascii_lowercase):
+    return ''.join(random.choice(chars) for _ in range(size))
+
+
+def scrape_gplay_to_db_by_search():
+    text_file = open("/Users/alonsinger/git/my-privacypolicy-thesis/book1.txt", "r")
+    lines = text_file.readlines()
+    text_file.close()
+    for line_ind in range(2649, len(lines)):
+        print("word with index {0} = {1}".format(line_ind, lines[line_ind]))
+        try:
+            db_rows = []
+            results = play_scraper.search(query=lines[line_ind])
+            for result in results:
+                url = result.get('developer_url')
+                pp_url = result.get('developer_pp_address')
+
+                db_row = [result.get('app_id'), result.get('developer_id'), result.get('category'), url, pp_url]
+                db_rows.append(db_row)
+            print("save results: {0}".format(len(results)))
+            db_utils.exec_command("INSERT INTO applications \
+                 (name,developer,category,dev_url, pp_url)\
+                 VALUES \
+                 (%s,%s,%s,%s,%s)", db_rows)
+            # print ("added rows {0} for category {1} collection {2} page {3} ".format(len(db_rows),category_value, collection_value,page))
+        except Exception as e:
+            print(e)
+            e = sys.exc_info()[0]
+
+
+def scrape_gplay_to_db():
+    categories = play_scraper.categories()
+    for collection_value in play_scraper.lists.COLLECTIONS:
+        print("fetching for {0}".format(collection_value))
+        for category_value in categories:
+            for page in range(400):
+                try:
+                    db_rows = []
+                    results = play_scraper.collection(collection=collection_value, category=category_value,
+                                                      detailed=True, page=page)
+                    for result in results:
+                        url = result.get('developer_url')
+                        pp_url = result.get('developer_pp_address')
+
+                        db_row = [result.get('app_id'), result.get('developer_id'), category_value, url, pp_url]
+                        db_rows.append(db_row)
+                    db_utils.exec_command("INSERT INTO applications \
+                         (name,developer,category,dev_url, pp_url)\
+                         VALUES \
+                         (%s,%s,%s,%s,%s)", db_rows)
+                    # print ("added rows {0} for category {1} collection {2} page {3} ".format(len(db_rows),category_value, collection_value,page))
+                except Exception as e:
+                    print(e)
+                    e = sys.exc_info()[0]
+
+
+# init_applications_table()
+# scrape_gplay_to_db()
+scrape_gplay_to_db_by_search()
+
+#
---
+++ src/server/utils/db/tools.py
@@ -0,0 +1,62 @@
+import logging
+import sys
+import psycopg2
+import psycopg2.extras
+import traceback
+
+working_db_name = "test1"
+working_db_host = "localhost"
+working_db_user = "postgres"
+working_db_password = "1"
+
+
+class DBUtils:
+
+    def __init__(self):
+        pass
+
+    def get_db_connection(self):
+        try:
+            conn = psycopg2.connect(
+                "dbname='{}' user='{}' host='{}' password='{}'".format(working_db_name, working_db_user,
+                                                                       working_db_host, working_db_password))
+            return conn
+        except:
+            traceback.print_exc()
+            logging.error(sys.exc_info()[0])
+
+    def exec_command(self, sql, value_list=None):
+        try:
+            conn = self.get_db_connection()
+            cur = conn.cursor()
+            if value_list is None:
+                cur.execute(sql)
+            else:
+                cur.executemany(sql, value_list)
+            cur.close()
+            conn.commit()
+        except Exception as e:
+            print(e)
+            logging.exception(sys.exc_info()[0])
+        finally:
+            if conn is not None:
+                conn.close()
+
+    def db_select(self, sql, value_list=None):
+        try:
+            conn = self.get_db_connection()
+            cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+            if value_list is None:
+                cur.execute(sql)
+            else:
+                cur.execute(sql, value_list)
+            rows = cur.fetchall()
+            return rows
+        except:
+            logging.exception(sys.exc_info()[0])
+        finally:
+            if conn is not None:
+                conn.close
+
+
+db_utils = DBUtils()
