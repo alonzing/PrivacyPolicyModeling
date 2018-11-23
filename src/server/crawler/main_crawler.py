5d1eb1ff4cc136f2501d9d2f43c6c77bab274f38
Eden <edenyefet@gmail.com>
2018-11-23 15:55:06 +0200

    Added multi-threading.

--- src/server/crawler/main_crawler.py
+++ src/server/crawler/main_crawler.py
@@ -1,6 +1,7 @@
 import random
 import string
 import sys
+import threading
 
 import play_scraper
 from play_scraper import lists
@@ -12,15 +13,39 @@ def id_generator(size=6, chars=string.ascii_lowercase):
     return ''.join(random.choice(chars) for _ in range(size))
 
 
+def _scrape_category(collection_value, categories):
+    for category_value in categories:
+        for page in range(400):
+            try:
+                db_rows = []
+                results = play_scraper.collection(collection=collection_value, category=category_value,
+                                                  detailed=True, page=page)
+                for result in results:
+                    url = result.get('developer_url')
+                    pp_url = result.get('developer_pp_address')
+
+                    db_row = [result.get('app_id'), result.get('developer_id'), category_value, url, pp_url]
+                    db_rows.append(db_row)
+                db_utils.exec_command('INSERT INTO applications \
+                     (name,developer,category,dev_url, pp_url)\
+                     VALUES \
+                     (%s,%s,%s,%s,%s)', db_rows)
+                # print ("added rows {0} for category {1} collection {2} page {3} ".format(len(db_rows), category_value,
+                #                                                                          collection_value, page))
+            except:
+                # Must NOT print anything to stdout from thread
+                pass
+
+
 def scrape_gplay_to_db_by_search():
     text_file = open("/Users/alonsinger/git/my-privacypolicy-thesis/book1.txt", "r")
     lines = text_file.readlines()
     text_file.close()
-    for line_ind in range(2649, len(lines)):
-        print("word with index {0} = {1}".format(line_ind, lines[line_ind]))
+    for line_index in range(2649, len(lines)):
+        print("word with index {0} = {1}".format(line_index, lines[line_index]))
         try:
             db_rows = []
-            results = play_scraper.search(query=lines[line_ind])
+            results = play_scraper.search(query=lines[line_index])
             for result in results:
                 url = result.get('developer_url')
                 pp_url = result.get('developer_pp_address')
@@ -32,7 +57,6 @@ def scrape_gplay_to_db_by_search():
                  (name,developer,category,dev_url, pp_url)\
                  VALUES \
                  (%s,%s,%s,%s,%s)", db_rows)
-            # print ("added rows {0} for category {1} collection {2} page {3} ".format(len(db_rows),category_value, collection_value,page))
         except Exception as e:
             print(e)
             e = sys.exc_info()[0]
@@ -40,29 +64,21 @@ def scrape_gplay_to_db_by_search():
 
 def scrape_gplay_to_db():
     categories = play_scraper.categories()
+    threads = []
     for collection_value in play_scraper.lists.COLLECTIONS:
-        print("fetching for {0}".format(collection_value))
-        for category_value in categories:
-            for page in range(400):
-                try:
-                    db_rows = []
-                    results = play_scraper.collection(collection=collection_value, category=category_value,
-                                                      detailed=True, page=page)
-                    for result in results:
-                        url = result.get('developer_url')
-                        pp_url = result.get('developer_pp_address')
+        print('fetching for {0}'.format(collection_value))
+        try:
+            collection_thread = threading.Thread(target=_scrape_category, args=(collection_value, categories,))
+            collection_thread.start()
+            print('Thread with collection value {0} has started.'.format(collection_value))
+            threads.append(collection_thread)
+        except threading.ThreadError:
+            print('Error starting thread with collection value {0}.'.format(collection_value))
 
-                        db_row = [result.get('app_id'), result.get('developer_id'), category_value, url, pp_url]
-                        db_rows.append(db_row)
-                    db_utils.exec_command("INSERT INTO applications \
-                         (name,developer,category,dev_url, pp_url)\
-                         VALUES \
-                         (%s,%s,%s,%s,%s)", db_rows)
-                    # print ("added rows {0} for category {1} collection {2} page {3} ".format(len(db_rows),category_value, collection_value,page))
-                except Exception as e:
-                    print(e)
-                    e = sys.exc_info()[0]
+    for collection_thread in threads:
+        collection_thread.join()
 
 
-scrape_gplay_to_db()
-# scrape_gplay_to_db_by_search()
+if __name__ == '__main__':
+    scrape_gplay_to_db()
+    # scrape_gplay_to_db_by_search()
