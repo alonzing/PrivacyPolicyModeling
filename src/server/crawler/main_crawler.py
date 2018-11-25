8656b7768755e6c68faba74c6db173fa21ee3937
Eden <edenyefet@gmail.com>
2018-11-25 13:14:00 +0200

    Added multi-threading as thread pool.

--- src/server/crawler/main_crawler.py
+++ src/server/crawler/main_crawler.py
@@ -2,6 +2,7 @@ import random
 import string
 import sys
 import threading
+from multiprocessing.pool import ThreadPool
 
 import play_scraper
 from play_scraper import lists
@@ -13,28 +14,29 @@ def id_generator(size=6, chars=string.ascii_lowercase):
     return ''.join(random.choice(chars) for _ in range(size))
 
 
-def _scrape_category(collection_value, categories):
-    for category_value in categories:
-        for page in range(400):
-            try:
-                db_rows = []
-                results = play_scraper.collection(collection=collection_value, category=category_value,
-                                                  detailed=True, page=page)
-                for result in results:
-                    url = result.get('developer_url')
-                    pp_url = result.get('developer_pp_address')
+def _scrape_category(collection_value, category_value):
+    # collection_value = collection_cat_value_tuple[0]
+    # category_value = collection_cat_value_tuple[1]
+    for page in range(400):
+        try:
+            db_rows = []
+            results = play_scraper.collection(collection=collection_value, category=category_value,
+                                              detailed=True, page=page)
+            for result in results:
+                url = result.get('developer_url')
+                pp_url = result.get('developer_pp_address')
 
-                    db_row = [result.get('app_id'), result.get('developer_id'), category_value, url, pp_url]
-                    db_rows.append(db_row)
-                db_utils.exec_command('INSERT INTO applications \
+                db_row = [result.get('app_id'), result.get('developer_id'), category_value, url, pp_url]
+                db_rows.append(db_row)
+            db_utils.exec_command('INSERT INTO applications \
                      (name,developer,category,dev_url, pp_url)\
                      VALUES \
                      (%s,%s,%s,%s,%s)', db_rows)
-                # print ("added rows {0} for category {1} collection {2} page {3} ".format(len(db_rows), category_value,
-                #                                                                          collection_value, page))
-            except:
-                # Must NOT print anything to stdout from thread
-                pass
+            # print ("added rows {0} for category {1} collection {2} page {3} ".format(len(db_rows), category_value,
+            #                                                                          collection_value, page))
+        except:
+            # Must NOT print anything to stdout from thread
+            pass
 
 
 def scrape_gplay_to_db_by_search():
@@ -62,23 +64,29 @@ def scrape_gplay_to_db_by_search():
             e = sys.exc_info()[0]
 
 
-def scrape_gplay_to_db():
-    categories = play_scraper.categories()
-    threads = []
-    for collection_value in play_scraper.lists.COLLECTIONS:
-        print('fetching for {0}'.format(collection_value))
-        try:
-            collection_thread = threading.Thread(target=_scrape_category, args=(collection_value, categories,))
-            collection_thread.start()
-            print('Thread with collection value {0} has started.'.format(collection_value))
-            threads.append(collection_thread)
-        except threading.ThreadError:
-            print('Error starting thread with collection value {0}.'.format(collection_value))
+def _on_finished_task(col_cat_tuple):
+    print('Task with collection value {} and category_value {} is done.'.format(col_cat_tuple[0], col_cat_tuple[1]))
 
-    for collection_thread in threads:
-        collection_thread.join()
+
+def scrape_gplay_to_db(num_of_threads=1):
+    pool = ThreadPool(num_of_threads)
+    categories = play_scraper.categories()
+    results = []
+    for collection_value in play_scraper.lists.COLLECTIONS:
+        for category_value in categories:
+            try:
+                results.append(pool.apply_async(_scrape_category, (collection_value, category_value),
+                                                callback=lambda _, __=(
+                                                    collection_value, category_value): _on_finished_task(
+                                                    (collection_value, category_value))))
+                print('An async scraping task with collection value {0} and category_value {1} has been added '
+                      'to the thread pool.'.format(collection_value, category_value))
+            except threading.ThreadError:
+                print('Error starting thread with collection value {0}.'.format(collection_value))
+    pool.close()
+    pool.join()
 
 
 if __name__ == '__main__':
-    scrape_gplay_to_db()
+    scrape_gplay_to_db(2)
     # scrape_gplay_to_db_by_search()
