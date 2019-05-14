d73537f86d71b33ceb082e9b5c8b6dd874b8e150
Eden <edenyefet@gmail.com>
2019-05-14 16:41:20 +0300

    json

--- node_js server/main.js
+++ node_js server/main.js
@@ -1,39 +1,57 @@
-// getRequest('http://localhost:3000/?pp_url=http://afrogfx.com/Appspoilcy/com.MuslimRefliction.Al.Adab.Al.Mufrad-privacy_policy.html', onResponse);
+// getRequest('http://localhost:3000/?pp_url=https://www.snappii.com/policy', onResponse);
 // http://localhost:3000/?pp_url=http://afrogfx.com/Appspoilcy/com.MuslimRefliction.Al.Adab.Al.Mufrad-privacy_policy.html
 
 function onResponse(response) {
-    let main_text = document.getElementById("main_text");
-    if (main_text == null) {
-        main_text = document.createElement("p");
-        main_text.setAttribute("class", 'text-justify');
-        main_text.setAttribute("id", "main_text");
-        document.getElementsByClassName('container')[0].appendChild(main_text);
-    }
-
-    main_text.innerText = response;
-    console.log(response);
+    let jsonContent = JSON.parse(response);
+    displayScore(jsonContent.score);
+    document.getElementById("pp_text").innerText = jsonContent.pp_text;
 }
 
-function x() {
-    let v = document.getElementById('pp_url_input').value;
-    if (v !== "") {
-        getRequest(document.getElementById('pp_url_input').value, onResponse);
-        document.getElementById('error_msg').innerText = '';
-
+function displayScore(score) {
+    let score_badge = document.getElementById("pp_score_badge");
+    score_badge.innerText = score;
+    let badge_class;
+    if (score >= 90) {
+        badge_class = "badge badge-success";
+    } else if (score >= 70) {
+        badge_class = "badge badge-warning";
     } else {
-        document.getElementById('error_msg').innerText = 'Please enter a valid URL, idiot.';
+        badge_class = "badge badge-danger";
     }
+    score_badge.setAttribute("class", badge_class);
+    document.getElementById("pp_score_header").innerHTML += "Score: "
+}
+
+function OnSubmit() {
+    let v = document.getElementById('pp_url_input').value;
+
+    // Mock
+    getRequest(document.getElementById('pp_url_input').value, onResponse);
+
+    // if (v !== "") {
+    //     getRequest(document.getElementById('pp_url_input').value, onResponse);
+    //     document.getElementById('error_msg').innerText = '';
+    //
+    // } else {
+    //     document.getElementById('error_msg').innerText = 'Please enter a valid URL, idiot.';
+    // }
+}
+
+function responseMock() {
+    return '{"pp_text":"PP TEXT PP TEXT PP TEXT PP TEXT PP TEXT PP TEXT PP TEXT PP TEXT PP TEXT PP TEXT PP TEXT PP TEXT PP TEXT PP TEXT PP TEXT PP TEXT PP TEXT PP TEXT PP TEXT PP TEXT PP TEXT PP TEXT PP TEXT PP TEXT PP TEXT PP TEXT PP TEXT PP TEXT PP TEXT PP TEXT PP TEXT PP TEXT PP TEXT PP TEXT PP TEXT PP TEXT PP TEXT PP TEXT PP TEXT PP TEXT PP TEXT PP TEXT PP TEXT PP TEXT PP TEXT PP TEXT PP TEXT PP TEXT PP TEXT PP TEXT PP TEXT PP TEXT PP TEXT PP TEXT PP TEXT PP TEXT PP TEXT PP TEXT PP TEXT PP TEXT PP TEXT PP TEXT PP TEXT ",' +
+        '"category":"CATEGORY_NAME", "score":"75", "uid": 1100}';
 }
 
 function getRequest(theUrl, callback) {
-    let xmlHttp = new XMLHttpRequest();
-
-    xmlHttp.open("GET", theUrl, true); // true for asynchronous
-    xmlHttp.onreadystatechange = function () {
-        if (xmlHttp.readyState === 4 && xmlHttp.status === 200) {
-            callback(xmlHttp.responseText);
-        }
-    };
-    xmlHttp.send();
+    callback(responseMock())
+    // let xmlHttp = new XMLHttpRequest();
+    //
+    // xmlHttp.open("GET", theUrl, true); // true for asynchronous
+    // xmlHttp.onreadystatechange = function () {
+    //     if (xmlHttp.readyState === 4 && xmlHttp.status === 200) {
+    //         callback(xmlHttp.responseText);
+    //     }
+    // };
+    // xmlHttp.send();
 }
 
