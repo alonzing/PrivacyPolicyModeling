909bfd98f9c281b38e1b890ce7703e7d5a9807c4
dantepli <dan932@gmail.com>
2019-05-14 18:42:36 +0300

    place holder for UI

--- src/server/management_utils/http_server.py
+++ src/server/management_utils/http_server.py
@@ -11,7 +11,9 @@ db_query_handler = http_server_db_handler.HttpServerDBHandler()
 def get_pp_paragraphs_by_url():
     url = request.args.get('url', default=None, type=str)
     paragraphs_records = db_query_handler.paragraph_by_url_query(url)
-    paragraph_list = [paragraph_record.get('paragraph') for paragraph_record in paragraphs_records]
+    paragraph_list = [dict(paragraph_record) for paragraph_record in paragraphs_records]
+    for response_dict in paragraph_list:
+        response_dict['index'] = int(response_dict['index'])
     response = json.dumps(paragraph_list)
     return response
 
@@ -23,14 +25,41 @@ def get_metadata_by_url():
     return json.dumps(metadata)
 
 
+def place_holder_response(url):
+    response = {
+        'app_category': 'kfir',
+        'dev_url': 'http://fs.com',
+        'num_of_paragraphs': 12,
+        'num_of_topics': 5,
+        'num_of_missing_paragraphs': 7,
+        'num_of_paragraphs_category_value': 12,
+        'num_of_topics_category_value': 10,
+        'num_of_missing_paragraphs_category_value': 5
+    }
+    paragraphs_records = db_query_handler.paragraph_by_url_query(url)
+    paragraph_list = [dict(paragraph_record) for paragraph_record in paragraphs_records]
+    for paragraph_dict in paragraph_list:
+        paragraph_dict['index'] = int(paragraph_dict['index'])
+        paragraph_dict.pop('id')
+        paragraph_dict.pop('privacy_policy_id')
+        paragraph_dict.pop('pp_url')
+    response['paragraphs'] = paragraph_list
+    return response
+
+
 @http_server.route('/pp-prediction', methods=['GET'])
 def get_pp_prediction_by_url():
     url = request.args.get('url', default=None, type=str)
-    rows = db_query_handler.prediction_query_by_url(url)
-    resp = [dict(row) for row in rows]
-    for response_dict in resp:
-        response_dict['index'] = int(response_dict['index'])
-    return json.dumps(resp)
+    response = place_holder_response(url)
+    return json.dumps(response)
+
+    # TODO: modeling
+
+    # rows = db_query_handler.prediction_query_by_url(url)
+    # resp = [dict(row) for row in rows]
+    # for response_dict in resp:
+    #     response_dict['index'] = int(response_dict['index'])
+    # return json.dumps(resp)
 
 
 if __name__ == '__main__':
