5735c861325624bb800fcf70642458a5d359686b
Eden <edenyefet@gmail.com>
2019-05-15 22:29:37 +0300

    ui

--- node_js server/main.js
+++ node_js server/main.js
@@ -2,11 +2,25 @@
 // http://localhost:3000/?pp_url=http://afrogfx.com/Appspoilcy/com.MuslimRefliction.Al.Adab.Al.Mufrad-privacy_policy.html
 
 
-function onResponse(response) {
-    let jsonContent = JSON.parse(response);
-    displayScore(jsonContent.score);
-    document.getElementById("pp_text_content").innerText = jsonContent.pp_text;
+function displayStatisticalDetails(response) {
+    document.getElementById("t_body_number_of_paragraphs_val").innerText = response.num_of_paragraphs;
+    document.getElementById("t_body_number_of_paragraphs_category_val").innerText = response.num_of_paragraphs_category_value;
+    document.getElementById("t_body_number_of_topics_val").innerText = response.num_of_topics;
+    document.getElementById("t_body_number_of_topics_category_val").innerText = response.num_of_topics_category_value;
+    document.getElementById("t_body_missing_material_paragraphs_val").innerText = response.num_of_missing_paragraphs;
+    document.getElementById("t_body_missing_material_paragraphs_category_val").innerText = response.num_of_missing_paragraphs_category_value;
+    document.getElementById("statistical_table").removeAttribute("hidden");
+}
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+async function onResponse(response) {
+    document.getElementById("spinner").setAttribute("hidden", "hidden");
 
+    displayScore(response.score);
+    displayParagraphs(response.paragraphs);
+    displayStatisticalDetails(response);
+    // Show or hide text button
     $('#pp_text_div').attr('class', 'd-block');
     $("#pp_text_collapse").on(('show.bs.collapse'), function () {
         $("#pp_text_collapse_button").attr('disabled', true);
@@ -17,6 +31,20 @@ function onResponse(response) {
     }).on(('hidden.bs.collapse'), function () {
         $("#pp_text_collapse_button").text('Show Text').attr('disabled', false);
     });
+}
+
+function displayParagraphs(paragraphs) {
+    let str = '';
+    let lastIndex = 0;
+    paragraphs.forEach(function (element) {
+        if (element["index"] < lastIndex) {
+            return;
+        }
+        lastIndex++;
+        str += element["paragraph"].replace(/\n*\n/gi, ".\n") + ".\n\n";
+    });
+
+    document.getElementById("pp_text_content").innerText = str;
 }
 
 function displayScore(score) {
@@ -37,9 +65,6 @@ function displayScore(score) {
 function OnSubmit() {
     let v = document.getElementById('pp_url_input').value;
 
-    // // Mock
-    // getRequest(document.getElementById('pp_url_input').value, onResponse);
-
     if (v !== "") {
         getRequest(document.getElementById('pp_url_input').value, onResponse);
         document.getElementById('error_msg').innerText = '';
@@ -49,30 +74,12 @@ function OnSubmit() {
     }
 }
 
-function responseMock() {
-    return '{"pp_text":"PP TEXT PP TEXT PP TEXT PP TEXT PP TEXT PP TEXT PP TEXT PP TEXT PP TEXT PP TEXT PP TEXT PP TEXT PP TEXT PP TEXT PP TEXT PP TEXT PP TEXT PP TEXT PP TEXT PP TEXT PP TEXT PP TEXT PP TEXT PP TEXT PP TEXT PP TEXT PP TEXT PP TEXT PP TEXT PP TEXT PP TEXT PP TEXT PP TEXT PP TEXT PP TEXT PP TEXT PP TEXT PP TEXT PP TEXT PP TEXT PP TEXT PP TEXT PP TEXT PP TEXT PP TEXT PP TEXT PP TEXT PP TEXT PP TEXT PP TEXT PP TEXT PP TEXT PP TEXT PP TEXT PP TEXT PP TEXT PP TEXT PP TEXT PP TEXT PP TEXT PP TEXT PP TEXT PP TEXT ",' +
-        '"category":"CATEGORY_NAME", "score":"50"}';
-}
 
 function getRequest(theUrl, callback) {
-    console.log('response');
-    // axios.get('http://www.google.com/')
-    //     .then(function (response) {
-    //         // handle success
-    //         console.log(response);
-    //     })
-    //     .catch(function (error) {
-    //         // handle error
-    //         console.log(error);
-    //     })
-    //     .finally(function () {
-    //         // always executed
-    //     });
-
-
     axios.get('http://127.0.0.1:5000/pp-prediction?url=' + theUrl)
-        .then(function (response) {
+        .then(async function (response) {
             // handle success
+            await sleep(5000);
             callback(response.data);
         })
         .catch(function (error) {
@@ -82,5 +89,6 @@ function getRequest(theUrl, callback) {
         .finally(function () {
             // always executed
         });
+    document.getElementById("spinner").removeAttribute("hidden");
 }
 
