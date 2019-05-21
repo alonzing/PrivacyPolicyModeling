8ecf0b0691258147017c9677e5282675e0900870
dantepli <dan932@gmail.com>
2019-05-21 17:54:04 +0300

    changes to json in place_holder

--- src/server/management_utils/http_server.py
+++ src/server/management_utils/http_server.py
@@ -32,23 +32,29 @@ def get_metadata_by_url():
 
 def place_holder_response(url):
     response = {
-        'app_category': 'kfir',
-        'dev_url': 'http://fs.com',
-        'num_of_paragraphs': 12,
-        'num_of_topics': 5,
-        'num_of_missing_paragraphs': 7,
-        'num_of_paragraphs_category_value': 12,
-        'num_of_topics_category_value': 10,
-        'num_of_missing_paragraphs_category_value': 5,
-        'score': 7
+        'table': [
+            {
+                'parameter': 'Number of Paragraphs',
+                'value': 12,
+                'categoryValue': 12
+            },
+            {
+                'parameter': 'Number of Topics',
+                'value': 5,
+                'categoryValue': 10
+            },
+            {
+                'parameter': 'Missing Material Paragraphs',
+                'value': 7,
+                'categoryValue': 5
+            }
+        ],
+        'score': 5
     }
     paragraphs_records = db_query_handler.paragraph_by_url_query(url)
-    paragraph_list = [dict(paragraph_record) for paragraph_record in paragraphs_records]
-    for paragraph_dict in paragraph_list:
-        paragraph_dict['index'] = int(paragraph_dict['index'])
-        paragraph_dict.pop('id')
-        paragraph_dict.pop('privacy_policy_id')
-        paragraph_dict.pop('pp_url')
+    paragraph_list = [{'index': int(paragraph_record['index']),
+                       'value': paragraph_record['paragraph'],
+                       'score': 0.3} for paragraph_record in paragraphs_records]
     response['paragraphs'] = paragraph_list
     return response
 
@@ -62,7 +68,7 @@ def get_pp_prediction_by_url():
         # http status was 200: OK
         cleaned_pp_records = clean_pp_html_records(url_record_http_ok)
         split_or_bypass_pp(cleaned_pp_records)
-    return json.dumps('temp')
+    return json.dumps(place_holder_response(url))
     # response = place_holder_response(url)
     # return json.dumps(response)
 
