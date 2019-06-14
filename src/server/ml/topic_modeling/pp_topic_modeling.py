ee56b762e8c380b8f7fe9feff42b5d6e080190ef
Eden Yefet <edenyefet@gmail.com>
2019-06-14 15:17:38 +0300

    Fixed memory leak

--- src/server/ml/topic_modeling/pp_topic_modeling.py
+++ src/server/ml/topic_modeling/pp_topic_modeling.py
@@ -1,13 +1,12 @@
 import os
 import shutil
 import time
-
 import graphlab as gl
 
 from src.server.utils.db.tools import db_utils
 
 
-def build_SFrame_from_db(script):
+def build_s_frame_from_db(script):
     paragraph_records = db_utils.db_select(script)
     paragraphs_list = []
 
@@ -90,7 +89,7 @@ def model_pp(sframe_raw_filename, sframe_filename, model_filename, predictions_f
              script=get_all_paragraphs_from_db(), single_predict=False, topic_count=100, save_from=0):
     if not os.path.exists(sframe_filename):
         print("Building SFrame file...")
-        sframe_raw = build_SFrame_from_db(script)
+        sframe_raw = build_s_frame_from_db(script)
         sframe_raw.save(sframe_raw_filename)
         sframe_for_modeling = build_docs_for_modeling(sframe_raw, sframe_raw_filename)
         sframe_for_modeling.save(sframe_filename)
@@ -108,7 +107,7 @@ def model_pp(sframe_raw_filename, sframe_filename, model_filename, predictions_f
 
     if not os.path.exists(predictions_filename) is None:
         print("Building predictions...")
-        docs_res = sframe_raw;
+        docs_res = sframe_raw
         docs_res['res'] = model.predict(sframe_for_modeling)
         docs_res['res_prob'] = model.predict(sframe_for_modeling, output_type='probability')
         docs_res.save(predictions_filename.format(topic_count))
@@ -120,23 +119,28 @@ def model_pp(sframe_raw_filename, sframe_filename, model_filename, predictions_f
     single_predict_rows = []
     db_rows = []
     records_count = 0
+
+    docs_res_res = docs_res['res']
+    docs_res_prob = docs_res['res_prob']
+    docs_parg = docs_res['X1']
     for i in range(save_from, total_docs):
-        db_row = [topic_count, \
-                  docs_res['res'][i], \
-                  docs_res['res_prob'][i][docs_res['res'][i]], \
-                  docs_res['X1'][i]]
+        x = docs_res_res[i]
+        db_row = [topic_count,
+                  x,
+                  docs_res_prob[i][x],
+                  docs_parg[i]]
+
         db_rows.append(db_row)
         if single_predict:
             single_predict_rows.append(db_row)
 
-        if records_count == 100 or i == total_docs - 1:
+        if records_count == 1000 or i == total_docs - 1:
             records_count = 0
             db_utils.exec_command("INSERT INTO privacy_policy_paragraphs_prediction \
                                 (running_id,topic_id,probability,paragraph)\
-                                                   VALUES \
-                                                    (%s,%s,%s,%s)", db_rows)
+                                                   VALUES (%s,%s,%s,%s)", db_rows)
             db_rows = []
-            time.sleep(1)
+            time.sleep(3)
             print("saved up to {} out of {}".format(i, total_docs))
         else:
             records_count += 1
@@ -156,7 +160,7 @@ def build_prediction_results(topic_count, model_file_name):
 
     results_html_file = open(root_results_dir + "/results.html", "w+")
     results_html_file.write("<html><table border='1'><tr><td>Topic Number</td><td>Words</td><td>Paragraphs</td></tr>")
-
+    print('started phase 1 of build predictions results')
     paragraphs_html_list = []
     for i in range(topic_count):
         paragraphs_html_list.append("<html><table border='1'>")
@@ -165,13 +169,17 @@ def build_prediction_results(topic_count, model_file_name):
         paragraphs_url = "<a href='./paragraphs_topic_{}.html'>paragraphs</a>".format(i)
         results_html_file.write(
             "<tr><td>{}</td><td>{}</td><td>{}</td></tr>".format(i, print_words_list, paragraphs_url))
+        print('{} out of {}'.format(i + 1, topic_count))
     results_html_file.write("</table></html>")
     results_html_file.close()
 
+    print('started phase 2 of build predictions results')
     for topic_id in range(0, topic_count):
         results_records = db_utils.db_select(
-            "select probability,paragraph from privacy_policy_paragraphs_prediction where running_id= {} and topic_id={} group by paragraph,probability order by probability desc".format(
-                topic_count, topic_id))
+            "select probability,paragraph from privacy_policy_paragraphs_prediction "
+            "where running_id= {} and topic_id={}"
+            "group by paragraph,probability"
+            "order by probability desc".format(topic_count, topic_id))
         topic_html = "<html><table border='1'><tr><td>Probability</td><td>Paragraph</td></tr>"
         for results_record in results_records:
             topic_html += "<tr><td>{:.4f}</td><td>{}</td></tr>".format(results_record.get('probability'),
@@ -179,7 +187,7 @@ def build_prediction_results(topic_count, model_file_name):
         topic_html += "</table></html>"
         paragraphs_html_file = open(root_results_dir + "/paragraphs_topic_{}.html".format(topic_id), "w+")
         paragraphs_html_file.write(topic_html)
-
+        print('{} out of {}'.format(topic_id + 1, topic_count))
     print("done")
 
 
@@ -195,7 +203,7 @@ def build_topics_models():
     working_dir = 'models_and_data{0}run_{1}'.format(os.path.sep, 'test')
     os.makedirs(working_dir)
     print('directory {0} was created'.format(working_dir))
-    sframe_raw_filename, sframe_filename, model_filename, predictions_filename =\
+    sframe_raw_filename, sframe_filename, model_filename, predictions_filename = \
         get_filenames(working_dir, working_dir, working_dir, working_dir)
     topic_count = model_pp(sframe_raw_filename, sframe_filename, model_filename, predictions_filename)
     build_prediction_results(topic_count, model_filename)
@@ -206,14 +214,16 @@ def build_from_exists_modeling(pp_url, pp_id):
     working_model_dir = 'models_and_data{0}run_{1}'.format(os.path.sep, 'test')
     if not os.path.exists(working_dir):
         os.makedirs(working_dir)
-    sframe_raw_filename, sframe_filename, model_filename, predictions_filename =\
+    sframe_raw_filename, sframe_filename, model_filename, predictions_filename = \
         get_filenames(working_dir, working_dir, working_model_dir, working_dir)
     script = get_paragraphs_from_db_for_single_pp_url(pp_url)
-    single_predict_rows = model_pp(sframe_raw_filename, sframe_filename, model_filename, predictions_filename, script, single_predict=True)
+    single_predict_rows = model_pp(sframe_raw_filename, sframe_filename, model_filename, predictions_filename, script,
+                                   single_predict=True)
     shutil.rmtree(working_dir)
     return single_predict_rows
 
 
-build_topics_models()
-# pp_url = 'http://christianchannel.us/privacy-policy/'
-# build_from_exists_modeling(pp_url, 1940)
+if __name__ == '__main__':
+    build_topics_models()
+    # pp_url = 'http://christianchannel.us/privacy-policy/'
+    # build_from_exists_modeling(pp_url, 1940)
